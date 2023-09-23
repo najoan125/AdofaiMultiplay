6d2298a11805bed31e06cb296919d7725acd057f
Najoan <68422289+najoan125@users.noreply.github.com>
2023-09-23 21:15:32 +0900

    초기 커밋.

---
+++ AdofaiMultiplay/Main.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityModManagerNet;
+
+namespace AdofaiMultiplay
+{
+    public static class Main
+    {
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static Harmony harmony;
+        public static bool IsEnabled = false;
+        public static bool isplaying = false;
+
+        public static void Setup(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+            modEntry.OnToggle = OnToggle;
+            modEntry.OnUpdate = OnUpdate;
+        }
+
+        private static void OnUpdate(UnityModManager.ModEntry modentry, float deltaTime)
+        {
+            if (!scrController.instance || !scrConductor.instance)
+            {
+                return;
+            }
+            isplaying = !scrController.instance.paused && scrConductor.instance.isGameWorld;
+        }
+
+        private static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            IsEnabled = value;
+
+            if (value)
+            {
+                harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            else
+            {
+                harmony.UnpatchAll(modEntry.Info.Id);
+            }
+            return true;
+        }
+    }
+}
